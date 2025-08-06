e64803f63cb764a9904a8921d8098b69a91a63ff
wdc <wdc>
2025-08-06 16:16:50 +0700

    fix version build

--- projects/pla-shared-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of pla-shared-lib
- */
-
-export * from './lib/pla-button-primary/pla-button-primary.component';
-export * from './lib/pla-shared-lib.service';
-export * from './lib/pla-shared-lib.component';
